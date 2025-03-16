42a409fd5be64dec0146b1788ecb70bc7d9bc6bc
Victor-sarti <Sartivictor24@gmail.com>
2025-03-16 20:40:40 -0300

    add turma controller

---
+++ src/repository/turma.repository.js
@@ -0,0 +1,48 @@
+import connection from "./connection.js";
+
+export async function inserir(turma) {
+    const comando = 
+        `INSERT INTO tb_turma(nm_turma, ds_curso, nr_ano_letivo,
+                qtd_capacidade, bt_ativo, dt_inclusao) 
+                    VALUES (?, ?, ?, ?, ?, ?) `
+    
+    let [info] = await connection.query(comando, [turma.nome, turma.desc, turma.anoletivo, turma.capacidade, turma.bt, turma.data]);
+    return info.insertId
+    
+}
+
+export async function alterar(id, turma) {
+const comando = `UPDATE tb_turma 
+                    SET nm_turma = ?,
+                       ds_curso = ?,
+                       nr_ano_letivo = ?,
+                       qtd_capacidade = ?,
+                       bt_ativo = ?,
+                       dt_inclusao = ? 
+                WHERE id_turma = ?`;
+
+let [info] = await connection.query(comando, [turma.nome, turma.desc, turma.anoletivo, turma.capacidade, turma.bt, turma.data, id]);
+ return info.affectedRows   
+}
+
+export async function remover(id) {
+    const comando = 
+    ` DELETE FROM tb_turma 
+      WHERE id_turma = ?`
+    let [info] = await connection.query(comando, [id]);
+     return info.affectedRows   
+    }
+
+    export async function listar() {
+        const comando = `SELECT id_turma as id,
+                               nm_turma as nome,
+                               ds_curso as ds,
+                               nr_ano_letivo as letivo,
+                               qtd_capacidade as capacidade,
+                               bt_ativo as bt,
+                               dt_inclusao as inclusao
+                        FROM tb_turma`;
+        
+        let [info] = await connection.query(comando);
+        return info; 
+    }
