19966c6946163633a2904e6391a0d9bd11e1d2ad
Victor-sarti <Sartivictor24@gmail.com>
2025-03-16 20:40:51 -0300

    add turma repo

---
+++ src/controller/turma.controller.js
@@ -0,0 +1,46 @@
+import {listar, inserir, alterar, remover} from '../repository/turma.repository.js'
+
+import { Router } from 'express'
+const endpoints = Router();
+
+
+endpoints.get('/turma', async (req, resp) => {
+  let registros = await listar();
+  resp.send(registros);
+})
+
+endpoints.post('/turma', async (req, resp) => {
+  let turma = req.body;
+
+  let novoId = await inserir(turma);
+  resp.send({ novoId })
+})
+
+
+endpoints.put('/turma/:id', async (req, resp) => {
+  let id = req.params.id;
+  let turma = req.body;
+
+  let linhasAfetadas = await alterar(id, turma);
+  resp.send({ linhasAfetadas })
+})
+
+
+
+endpoints.delete('/turma/:id', async (req, resp) => {
+  let id = req.params.id;
+
+  let linhasAfetadas = await remover(id);
+  if (linhasAfetadas == 0) {
+    return resp.status(404)
+               .send({ erro: 'Registro não encontrado.' })
+  }
+
+
+  resp.send({ linhasAfetadas })
+})
+
+
+
+
+export default endpoints;
