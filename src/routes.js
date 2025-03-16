0a1432fb6ba7b35741fdc912b6dd6bc399d0702e
Victor-sarti <Sartivictor24@gmail.com>
2025-03-16 20:40:10 -0300

    add route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import aluno from './controller/alunoController.js'
 import ator from './controller/atorController.js'
+import turma from './controller/turma.controller.js'
 
 export default function adicionarRotas(servidor) {
   servidor.use(aluno);
   servidor.use(ator);
+  servidor.use(turma);
 }
